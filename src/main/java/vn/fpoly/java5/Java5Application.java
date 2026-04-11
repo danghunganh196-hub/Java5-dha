bcf262c295c7bcac40778b2a89549228c26fe38a
dha.necato <danghunganh196@gmail.com>
2026-04-11 14:23:15 +0700

    init thich thi commit push

--- src/main/java/vn/fpoly/java5/Java5Application.java
+++ src/main/java/vn/fpoly/java5/Java5Application.java
@@ -2,7 +2,9 @@ package vn.fpoly.java5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class Java5Application {
 
